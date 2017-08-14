9941d1273c29e7f0d74bd84d0444d87407ef6704
Luiz <luiz.earaujo@gmail.com>
2017-08-14 08:34:42 -0300

    Implementação inicial do bitnoitfy

--- src/BitNotify/Form1.Designer.cs
+++ src/BitNotify/Form1.Designer.cs
@@ -31,6 +31,13 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.mynotifyicon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.txTempo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbExchange = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btSair = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // mynotifyicon
@@ -40,22 +47,97 @@
             this.mynotifyicon.Visible = true;
             this.mynotifyicon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.mynotifyicon_MouseDoubleClick);
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Atualização";
+            // 
+            // txTempo
+            // 
+            this.txTempo.Location = new System.Drawing.Point(95, 13);
+            this.txTempo.Name = "txTempo";
+            this.txTempo.Size = new System.Drawing.Size(100, 20);
+            this.txTempo.TabIndex = 1;
+            this.txTempo.Text = "60";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Exchange";
+            // 
+            // cbExchange
+            // 
+            this.cbExchange.FormattingEnabled = true;
+            this.cbExchange.Location = new System.Drawing.Point(95, 46);
+            this.cbExchange.Name = "cbExchange";
+            this.cbExchange.Size = new System.Drawing.Size(100, 21);
+            this.cbExchange.TabIndex = 3;
+            this.cbExchange.Text = "Foxbit";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 115);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btSair
+            // 
+            this.btSair.Location = new System.Drawing.Point(150, 115);
+            this.btSair.Name = "btSair";
+            this.btSair.Size = new System.Drawing.Size(75, 23);
+            this.btSair.TabIndex = 5;
+            this.btSair.Text = "Sair";
+            this.btSair.UseVisualStyleBackColor = true;
+            this.btSair.Click += new System.EventHandler(this.btSair_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(257, 234);
+            this.Controls.Add(this.btSair);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cbExchange);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txTempo);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "BitNotify";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Resize += new System.EventHandler(this.Form1_Resize);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.NotifyIcon mynotifyicon;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txTempo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cbExchange;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btSair;
     }
 }
 
--- src/BitNotify/Form1.cs
+++ src/BitNotify/Form1.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using BitInformation;
+using System;
 using System.Windows.Forms;
 
 namespace BitNotify
 {
     public partial class Form1 : Form
     {
+
+        public string Message { get; set; }
+        private double Ultimo { get; set; }
+        private bool Atualiza { get; set; } = true;
+
         public Form1()
         {
             InitializeComponent();
@@ -12,19 +18,19 @@ namespace BitNotify
 
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            
         }
 
         private void Form1_Resize(object sender, EventArgs e)
         {
-            mynotifyicon.BalloonTipTitle = "My application still working...";
-            mynotifyicon.BalloonTipText = "My Sample Application";
+            mynotifyicon.BalloonTipTitle = "BitNotify (Cotação FoxBit)";
+            mynotifyicon.BalloonTipText = Message;
 
             if (FormWindowState.Minimized == this.WindowState)
             {
               
                 mynotifyicon.Visible = true;
-                mynotifyicon.ShowBalloonTip(500);
+                mynotifyicon.ShowBalloonTip(1000);
                 this.Hide();
             }
             else if (FormWindowState.Normal == this.WindowState)
@@ -36,6 +42,62 @@ namespace BitNotify
         private void mynotifyicon_MouseDoubleClick(object sender, MouseEventArgs e)
         {
             this.Show();
+        }
+
+        private void Get()
+        {
+            var repo = new Repositorio();
+            var root = repo.Get();
+
+            if (Ultimo != root.ticker_1h.exchanges.FOX.last)
+            {
+                Atualiza = true;
+            }
+            
+
+            Message = $@"ÚLTIMO: {root.ticker_1h.exchanges.FOX.last.ToString("N")}
+ANTERIOR: {Ultimo.ToString("N")}
+MÍN: {root.ticker_1h.exchanges.FOX.low.ToString("N")}
+MÁX: {root.ticker_1h.exchanges.FOX.high.ToString("N")}
+USD COM: {root.rates.USDCBRL.ToString("N")}
+USD TUR: {root.rates.USDTBRL.ToString("N")}";
+
+            Ultimo = root.ticker_1h.exchanges.FOX.last;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Get();
+
+
+            if (Atualiza)
+            {
+                mynotifyicon.Visible = true;
+                mynotifyicon.ShowBalloonTip(1000);
+                Atualiza = false;
+            }
+            
+        }
+
+        private void btSair_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Get();
+
+            mynotifyicon.BalloonTipTitle = "BitNotify (Cotação FoxBit)";
+            mynotifyicon.BalloonTipText = Message;
+
+            mynotifyicon.Visible = true;
+            mynotifyicon.ShowBalloonTip(1000);
+            this.Hide();
+
+            var segundos = Convert.ToInt32(txTempo.Text);
+            timer1.Interval = 1000 * segundos;
+            timer1.Start();
         }
     }
 }
